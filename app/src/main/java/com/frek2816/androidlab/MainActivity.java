214f650fefd122dafb0769b954e773ea14336188
frek2 <frek2816@gmail.com>
2018-12-08 21:31:55 +0200

    Finish lab 6

--- app/src/main/java/com/frek2816/androidlab/MainActivity.java
+++ app/src/main/java/com/frek2816/androidlab/MainActivity.java
@@ -1,27 +1,50 @@
 package com.frek2816.androidlab;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
     private List<ImageItem> imageList;
     private ImageAdapter recyclerAdapter;
+
+    @BindView(R.id.toolbar)
+    protected Toolbar toolbar;
+
+    @BindView(R.id.swipe_refresh_layout)
+    protected SwipeRefreshLayout swipeRefreshLayout;
+
+    @BindView(R.id.no_data_image)
+    protected ImageView noDataImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        noDataImage.setVisibility(View.INVISIBLE);
+
         setRecyclerAdapter();
+        refreshPageContent();
     }
 
     @Override
@@ -29,6 +52,17 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         setJsonToRecyclerAdapter();
     }
+
+    private void refreshPageContent() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                setJsonToRecyclerAdapter();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
 
     private void setRecyclerAdapter(){
         imageList = new ArrayList<>();
@@ -50,12 +84,14 @@ public class MainActivity extends AppCompatActivity {
                 assert response.body() != null;
                 imageList = response.body();
                 recyclerAdapter.setImageList(imageList);
+                noDataImage.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onFailure(Call<List<ImageItem>> call, Throwable t) {
+                noDataImage.setVisibility(View.VISIBLE);
                 Toast.makeText(MainActivity.this,
-                        "Something went wrong...Error message: "
+                        getString(R.string.message)
                                 + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
