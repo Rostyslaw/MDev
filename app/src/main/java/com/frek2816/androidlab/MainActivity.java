3bb772d66c9ef99ed61df0f18489cd8ba8fef779
Rostyslaw <frek2816@gmail.com>
2018-12-09 22:50:52 +0200

    Revert "Lab 8"

--- app/src/main/java/com/frek2816/androidlab/MainActivity.java
+++ app/src/main/java/com/frek2816/androidlab/MainActivity.java
@@ -1,41 +1,63 @@
 package com.frek2816.androidlab;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String DETAILS = "fragment_details";
-    public static final String FAVOURITES = "favourites";
-
-    @BindView(R.id.toolbar)
-    protected Toolbar toolbar;
-
-    @BindView(R.id.favourites_button)
-    protected FloatingActionButton goToFavouritesButton;
+    private List<ImageItem> imageList;
+    private ImageAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
-        ManageFragments manageFragments = new ManageFragments(this);
-        ApplicationEx.getInstance().setFragmentManager(manageFragments);
-        ApplicationEx.getInstance().getFragmentManager().setFragment(new ImageListFragment());
-
+        setRecyclerAdapter();
     }
 
-    /*@OnClick(R.id.favourites_button)
-    protected void onFavouritesButtonClick(View view){
-        setFragment(new ImageFavouritesFragment());
-    }*/
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setJsonToRecyclerAdapter();
+    }
 
+    private void setRecyclerAdapter(){
+        imageList = new ArrayList<>();
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerAdapter = new ImageAdapter(getApplicationContext(), imageList);
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    private void setJsonToRecyclerAdapter() {
+        GetImageDataService service = RetrofitInstance
+                .getRetrofitInstance().create(GetImageDataService.class);
+        Call<List<ImageItem>> call = service.getImageList();
+
+        call.enqueue(new Callback<List<ImageItem>>() {
+            @Override
+            public void onResponse(Call<List<ImageItem>> call, Response<List<ImageItem>> response) {
+                assert response.body() != null;
+                imageList = response.body();
+                recyclerAdapter.setImageList(imageList);
+            }
+
+            @Override
+            public void onFailure(Call<List<ImageItem>> call, Throwable t) {
+                Toast.makeText(MainActivity.this,
+                        "Something went wrong...Error message: "
+                                + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
