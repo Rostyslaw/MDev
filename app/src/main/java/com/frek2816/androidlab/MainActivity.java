e4970509f9e25ad64e488a50a57bce88893e37b8
frek2 <frek2816@gmail.com>
2018-12-10 01:26:34 +0200

    Add lab_7

--- app/src/main/java/com/frek2816/androidlab/MainActivity.java
+++ app/src/main/java/com/frek2816/androidlab/MainActivity.java
@@ -1,39 +1,25 @@
 package com.frek2816.androidlab;
 
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    private List<ImageItem> imageList;
-    private ImageAdapter recyclerAdapter;
+    public static final String DETAILS = "fragment_details";
+    public static final String FAVOURITES = "favourites";
 
     @BindView(R.id.toolbar)
     protected Toolbar toolbar;
 
-    @BindView(R.id.swipe_refresh_layout)
-    protected SwipeRefreshLayout swipeRefreshLayout;
-
-    @BindView(R.id.no_data_image)
-    protected ImageView noDataImage;
-
+    @BindView(R.id.favourites_button)
+    protected FloatingActionButton goToFavouritesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,59 +27,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-        noDataImage.setVisibility(View.INVISIBLE);
 
-        setRecyclerAdapter();
-        refreshPageContent();
+        setFragment(new ImageListFragment());
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setJsonToRecyclerAdapter();
+    @OnClick(R.id.favourites_button)
+    protected void onFavouritesButtonClick(View view){
+        setFragment(new ImageFavouritesFragment());
     }
 
-    private void refreshPageContent() {
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                setJsonToRecyclerAdapter();
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        });
-    }
-
-
-    private void setRecyclerAdapter(){
-        imageList = new ArrayList<>();
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerAdapter = new ImageAdapter(getApplicationContext(), imageList);
-        recyclerView.setAdapter(recyclerAdapter);
-    }
-
-    private void setJsonToRecyclerAdapter() {
-        GetImageDataService service = RetrofitInstance
-                .getRetrofitInstance().create(GetImageDataService.class);
-        Call<List<ImageItem>> call = service.getImageList();
-
-        call.enqueue(new Callback<List<ImageItem>>() {
-            @Override
-            public void onResponse(Call<List<ImageItem>> call, Response<List<ImageItem>> response) {
-                assert response.body() != null;
-                imageList = response.body();
-                recyclerAdapter.setImageList(imageList);
-                noDataImage.setVisibility(View.INVISIBLE);
-            }
-
-            @Override
-            public void onFailure(Call<List<ImageItem>> call, Throwable t) {
-                noDataImage.setVisibility(View.VISIBLE);
-                Toast.makeText(MainActivity.this,
-                        getString(R.string.message)
-                                + t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void setFragment(final Fragment fragment) {
+        getSupportFragmentManager().
+                beginTransaction()
+                .replace(R.id.frame_container, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
